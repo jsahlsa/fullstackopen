47c5577bc4950d0cd7bc274e0277e790941dd44c
jsahlsa <joe.sahlstrom@gmail.com>
2025-12-01 15:47:05 -0800

    finished part 2.4

--- part2/courseinfo/src/App.jsx
+++ part2/courseinfo/src/App.jsx
@@ -20,7 +20,7 @@ const Content = (props) => {
     return (
         <div>
             {props.course.parts.map((part, i) =>
-            <Part key={i} part={part.name} exercise={part.exercises} />
+            <Part key={part.id} part={part.name} exercise={part.exercises} />
             )}
         </div>
     )
@@ -40,7 +40,6 @@ const Total = (props) => {
 }
 
 const Course = (props) => {
-    console.log('course', props)
     return (
         <>
             <Header course={props.course} />
@@ -51,31 +50,53 @@ const Course = (props) => {
 }
 
 const App = () => {
-    const course = {
-        name: 'Half Stack application development',
-        parts:  [
-            {
-                name: 'Fundamentals of React',
-                exercises: 10
-            },
-            {
+    const courses = [
+        {
+            name: 'Half Stack application development',
+            id: 1,
+            parts:  [
+                {
+                    id: 1,
+                    name: 'Fundamentals of React',
+                    exercises: 10
+                },
+                {
+                    id: 2,
+                    name: 'Using props to pass data',
+                    exercises: 7
+                },
+                {
+                    id: 3,
+                    name: 'State of a component',
+                    exercises: 14
+                },
+                {
+                    id: 4,
+                    name: 'Redux',
+                    exercises: 11
+                }
+            ]
+        },
+        {
+            name: 'Node.js',
+            id: 2,
+            parts: [
+                {
+                    id: 1,
+                    name: 'Routing',
+                    exercises: 3
+                },
+                {
+                    id: 2,
+                    name: 'Middlewares',
+                    exercises: 7
+                }
+            ]
+        }]
 
-                name: 'Using props to pass data',
-                exercises: 7
-            },
-            {
-
-                name: 'State of a component',
-                exercises: 14
-            },
-            {
-                name: 'Redux',
-                exercises: 11
-            }
-        ]
-    }
-
-    return <Course course={course} />
+    return (
+        courses.map(course => <Course key={course.id} course={course} />)
+    )
 }
 
 export default App
