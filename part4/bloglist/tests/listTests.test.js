1200bb751713f7e870a2b0791fbb74a0fa62bcaf
jsahlsa <joe.sahlstrom@gmail.com>
2025-12-23 15:44:18 -0800

    finished 4.7

--- part4/bloglist/tests/listTests.test.js
+++ part4/bloglist/tests/listTests.test.js
@@ -8,7 +8,8 @@ const {
     totalLikes,
     dummy,
     favoriteBlog,
-    mostBlogs
+    mostBlogs,
+    mostLikes
 } = require('../utils/list_helper')
 
 describe('dummy', () => {
@@ -125,7 +126,10 @@ describe('favorite blog', () => {
         "id": "6945b2ffa2a3a8836d1a61b8"
     }
 
-    assert.deepStrictEqual(favoriteBlog(listWithAllBlogs), blogWithMost)
+
+    test('returns favorite blog', () => {
+        assert.deepStrictEqual(favoriteBlog(listWithAllBlogs), blogWithMost)
+    })
 })
 
 describe('most blogs', () => {
@@ -167,5 +171,56 @@ describe('most blogs', () => {
         "id": "6945b35ba2a3a8836d1a61bc"
     }]
 
-    assert.strictEqual(mostBlogs(listWithAllBlogs), 'Joe Sahlsa')
+    test('author with most blogs', () => {
+        assert.strictEqual(mostBlogs(listWithAllBlogs), 'Joe Sahlsa')
+    })
+})
+
+describe('most likes', () => {
+    const listWithAllBlogs = [{
+        "title": "Fairplay app",
+        "author": "Joe Sahlsa",
+        "url": "https://joesahlsa.dev/blog/fairplay-app/",
+        "likes": 3,
+        "id": "69449e4da2a3a8836d1a61b1"
+    }, {
+        "title": "Custom language syntax highlighting",
+        "author": "Joe Sahlsa",
+        "url": "https://joesahlsa.dev/blog/custom-language-syntax-highlighting/",
+        "likes": 1,
+        "id": "69449e75a2a3a8836d1a61b3"
+    }, {
+        "title": "Wrapping up 2025",
+        "author": "Andy Bell",
+        "url": "https://piccalil.li/blog/wrapping-up-2025/",
+        "likes": 4,
+        "id": "6945b2c0a2a3a8836d1a61b6"
+    }, {
+        "title": "Programming principles for self taught front-end developers",
+        "author": "Kilian Valkhof",
+        "url": "https://piccalil.li/blog/programming-principles-for-self-taught-front-end-developers/",
+        "likes": 11,
+        "id": "6945b2ffa2a3a8836d1a61b8"
+    }, {
+        "title": "Brand New Layouts with CSS Subgrid",
+        "author": "Josh Comeau",
+        "url": "https://www.joshwcomeau.com/css/subgrid/",
+        "likes": 7,
+        "id": "6945b32da2a3a8836d1a61ba"
+    }, {
+        "title": "An Interactive Guide to SVG Paths",
+        "author": "Josh Comeau",
+        "url": "https://www.joshwcomeau.com/svg/interactive-guide-to-paths/",
+        "likes": 55,
+        "id": "6945b35ba2a3a8836d1a61bc"
+    }]
+
+    const authorMostLikes = {
+        author: 'Josh Comeau',
+        likes: 62
+    }
+
+    test('author with most likes', () => {
+        assert.deepStrictEqual(mostLikes(listWithAllBlogs), authorMostLikes)
+    })
 })
--- part4/bloglist/utils/list_helper.js
+++ part4/bloglist/utils/list_helper.js
@@ -22,8 +22,47 @@ const mostBlogs = (blogs) => {
     return keyOfHighestValue
 }
 
+const mostLikes = (blogs) => {
+    const likesObject = makeLikesObject(blogs, 'author', 'likes')
+    const highestLikes = getHighestLikes(likesObject)
+    const objectWithHighestLikes = getObjectMostLikes(likesObject, highestLikes)
+    console.log(objectWithHighestLikes)
+    return objectWithHighestLikes
+}
+
 // helper functions
 //
+const getObjectMostLikes = (arr, val) => {
+    return arr.find(n => n.likes === val)
+}
+//
+const getHighestLikes = (arr) => {
+    return Math.max(...arr.map(n => n.likes))
+
+}
+//
+// Create object that looks up first param and tallies second param, returns arrray of objects
+const makeLikesObject = (obj, lookup, tally) => {
+    const reducer = (acc, item) => {
+        if (!acc.find(n => n.author === item.author)) {
+            const newPerson = {
+                author: item.author,
+                likes: item.likes
+            }
+
+            acc.push(newPerson)
+        } else {
+            const index = acc.findIndex(n => n.author === item.author)
+            acc[index].likes += item.likes
+        }
+
+        return acc
+    }
+    const authorLikesSums = obj.reduce(reducer, [])
+    return authorLikesSums
+}
+
+
 // Get counts of keys value based on how many times it occurs in object
 const makeCountsOfKey = (obj, key) => {
     const reducer = (acc, item) => {
@@ -52,5 +91,6 @@ module.exports = {
     dummy,
     totalLikes,
     favoriteBlog,
-    mostBlogs
+    mostBlogs,
+    mostLikes
 }
