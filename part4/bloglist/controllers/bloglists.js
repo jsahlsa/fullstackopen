c39976ce67df395a88105d7c700b5c7d44ba2382
jsahlsa <joe.sahlstrom@gmail.com>
2026-01-05 16:41:01 -0800

    finished 4.17

--- part4/bloglist/controllers/bloglists.js
+++ part4/bloglist/controllers/bloglists.js
@@ -1,22 +1,38 @@
 const blogListRouter = require('express').Router()
-const BlogList = require('../models/bloglist.js')
+const BlogList = require('../models/bloglist')
+const User = require('../models/user')
 
 blogListRouter.get('/', async (request, response, next) => {
-    const blogs = await BlogList.find({})
+    const blogs = await BlogList.find({}).populate('user', {
+        username: 1,
+        name: 1
+    })
     response.json(blogs)
 })
 
 blogListRouter.post('/', async (request, response, next) => {
     const body = request.body
 
+    const user = await User.findById(body.userId)
+
+    if (!user) {
+        return response.status(400).json({
+            error: 'user does not exist'
+        })
+    }
+
     const blog = new BlogList({
         title: body.title,
         author: body.author,
         url: body.url,
-        likes: body.likes || 0
+        likes: body.likes || 0,
+        user: user._id
     })
 
     const savedBlog = await blog.save()
+    user.blogs = user.blogs.concat(savedBlog._id)
+    await user.save()
+
     response.status(201).json(savedBlog)
 })
 
